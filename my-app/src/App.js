b7864afe896345375d33f59d9fda4d199aa20155
sdd <1260064942@qq.com>
2018-09-15 16:10:06 +0800

    16:10

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,35 @@
+ /* jshint esversion: 6 */ 
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Class1 from './component/Class1';
+import Class2 from './component/Class2';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        {/* <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    </p> */}
+        <div>
+          {/* <Link to="./class1">class1</Link>
+          <Link to="class2">class2</Link> */}
+          <Link to="./">class1</Link>
+          <Link to="class2">class2</Link> 
+          {/* <Route path="/class1" component={Class1} />
+          <Route path="/class2" component={Class2} /> */}
+          <Route exact path="/" component={Class1} />
+          <Route path="/class2" component={Class2} />
+        </div>
       </div>
+      </Router>
     );
   }
 }
---
+++ my-app/src/component/Class2.js
@@ -0,0 +1,12 @@
+ /* jshint esversion: 6 */ 
+import React, { Component } from 'react'
+
+export default class Class1 extends Component {
+    render() {
+        return (
+        <div>
+            <h1>Class2</h1>
+        </div>
+        )
+    }
+}
